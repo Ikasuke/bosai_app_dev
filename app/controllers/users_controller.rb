d53b23cd104f2e67e4a2c5b524f5da46ef028194
井加田 洸輔 <ika_3_ika@yahoo.co.jp>
2018-12-30 01:30:10 +0900

    profile画面のリマインドメール部分の部分を完成させた remindmailのvalidation validationの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class UsersController < ApplicationController
   def profile
     @user = current_user
@@ -5,19 +6,43 @@ class UsersController < ApplicationController
     remindmails = current_user.remindmails
     if remindmails.empty?
       # 新規に作るようにする controllerではなにもしない
+      @loginmail = false  # ログイン用メールアドレスをリマインドメールに使用しているか？=> していないのでfalse
     else
       @remindmails = remindmails
+      @loginmail = false   # ログイン用メールアドレスをリマインドメールに使用しているか？ trueなら使用している
+      @remindmails.each do |remindmail|
+        if remindmail.remind_email == @user.email
+          @loginmail = true
+          @loginmail_id = remindmail.id
+        else
+        end
+      end
     end
-  end
+    if params[:u_error_messages].nil?
+      @u_error_details = {key: "no_error"}   #ダミーのkeyとvalueを入れておく エラー防止
+    else
+      #validationのエラー表示用
+      @u_error_messages = params[:u_error_messages]   # エラーのメッセージ
+      @u_error_details = params[:u_error_details]     #エラーが表示された部分が格納
+    end
+    if params[:mail_error_messages].nil?
+      @mail_error_details = {key: "no_error"}   #ダミーのkeyとvalueを入れておく エラー防止
+    else
+      #validationのエラー表示用
+      @mail_error_messages = params[:mail_error_messages]   # エラーのメッセージ
+      @mail_error_details = params[:mail_error_details]     #エラーが表示された部分が格納
+      @mail_error_state = params[:mail_error_state]        # create => 新規作成　id => idに該当するアドレスのアップデート　でエラー
+    end
+  end # profile end
 
   def update
     @user = current_user
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to home_url, notice: "User was successfully updated." }
+        format.html { redirect_to home_url, notice: "正常にアップデートされました" }
         #format.json { render :show, status: :ok, location: @user }
       else
-        format.html { redirect_to home_url, error: "User was failed to be updated." }
+        format.html { redirect_to user_profile_path(u_error_messages: @user.errors.messages, u_error_details: @user.errors.details), flash: {error: "アップデートできませんでした"} }
         #format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
+# encoding: utf-8
 # frozen_string_literal: true
 
 module ApplicationHelper
-  APP_NAME = '防災ストッカー'   #防災アプリ
+  APP_NAME = "防災ストッカー"   #防災アプリ
 
   def flash_message(message, klass)
     content_tag(:div, class: "alert alert-#{klass}") do
-      concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+      concat content_tag(:button, "x", class: "close", data: {dismiss: "alert"})
       concat raw(message)
     end
   end
-
 end
